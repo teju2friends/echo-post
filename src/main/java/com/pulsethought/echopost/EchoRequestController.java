92a22e59b098fa210ba1924a17ad85adaa992ae3
Tejas jain <teju2friends@gmail.com>
2017-09-12 09:42:43 -0700

    Fixed imports

--- src/main/java/com/pulsethought/echopost/EchoRequestController.java
+++ src/main/java/com/pulsethought/echopost/EchoRequestController.java
@@ -1,9 +1,7 @@
 package com.pulsethought.echopost;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
